b1c520f1cb273b457e4ffec33f75a9e3249ba1e8
arthmont <arthurdesouzamonteiro@gmail.com>
2024-04-05 15:21:19 -0300

    troca para neondb finalizada

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,12 +1,21 @@
-import { PrismaClient } from "@prisma/client"
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv'
+import ws from 'ws'
+
+dotenv.config()
+neonConfig.webSocketConstructor = ws // Configuração do WebSocket para o Neon DB
+
+const connectionString = `${process.env.DATABASE_URL}`
+const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon(pool)
 
 declare global {
-  var prisma: PrismaClient | undefined
+ var prisma: PrismaClient | undefined
 }
 
-const prismadb = globalThis.prisma || new PrismaClient()
+const prismadb = globalThis.prisma || new PrismaClient({ adapter })
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
 
 export default prismadb;
-
-
